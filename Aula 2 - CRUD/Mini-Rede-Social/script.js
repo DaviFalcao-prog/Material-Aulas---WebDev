376c2af82891ac48409cbdb87e4cca73e2674446
DaviFalcao-prog <davifalcao1609@outlook.com>
2025-08-12 22:36:57 -0300

    Crude aula1

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,45 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+window.onload = function (){
+    mostrarPost();
+
+    document.querySelector("#postForm").addEventListener('submit',addPost)
+}
+
+function addPost(infosdoEvento){
+    infosdoEvento.preventDefault()
+    const textoPost = document.querySelector("#postText").value;
+    const categoriaPost = document.querySelector("#postCategory").value;
+    const imgPost = document.querySelector("#postImage")
+
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imgPost,
+        date: new Date().toLocaleDateString()
+    }
+    posts.unshift(novoPost);
+
+    mostrarPost()
+}
+
+function mostrarPost(){
+    //pega elemento que tera a lista de posts no HTML
+    const listaPosts = document.querySelector("#postList")
+    listaPosts.innerHTML = ""
+    posts.forEach(pegaItem => {
+        const cardPost = document.createElement("div")
+
+        cardPost.innerHTML = `
+        <h2>${pegaItem.text}</h2>
+        ${pegaItem.image? `<img src=${pegaItem.image}/>`:""}
+        <p>${pegaItem.category}</p>
+        <p>${pegaItem.date}</p>
+        <button>Editar</button>
+        <button>Apagar</button>
+        `
+
+    listaPosts.append(cardPost)
+    })
+}
